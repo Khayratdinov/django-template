4ce0092981ddad9f0d55b048045ca6f4d5bfbed3
Khayratdinov Nurpolat <Khayratdinov.np@gmail.com>
2023-02-16 12:16:07 +0500

    chore: ⛓ updated the pagination utils

--- project/apps/core/utils.py
+++ project/apps/core/utils.py
@@ -1,8 +1,12 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def paginate_queryset(request, queryset, per_page=10, page_kwarg="page"):
-    paginator = Paginator(queryset, per_page)
+def paginate_queryset(request, queryset, per_page=12, page_kwarg="page", order_by=None):
+    if order_by:
+        ordered_queryset = queryset.order_by(order_by)
+    else:
+        ordered_queryset = queryset
+    paginator = Paginator(ordered_queryset, per_page)
     page = request.GET.get(page_kwarg)
     try:
         page_obj = paginator.page(page)
